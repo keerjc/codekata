4719497db08d3574a3ec5243c3ff5091d3b5fff3
Keerthana J <keerthanajohns@gmail.com>
2017-06-20 16:56:44 +0530

    Create DigCount.java

---
+++ DigCount.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+public class DigCount
+{
+ public static void main(String[] args)
+ {
+  System.out.println("  *DIGIT COUNT*");
+  Scanner s=new Scanner(System.in);
+  System.out.println("Enter a value");
+  int no=s.nextInt();
+  int count=Integer.toString(no).length();
+  System.out.println("The sum of n values is " + count);
+ }
+}
