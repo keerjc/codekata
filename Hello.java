585e60c33ed5763d7b19950c2f946ac3b4581ad2
Keerthana J <keerthanajohns@gmail.com>
2017-06-20 16:37:01 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+public class Hello
+{
+ public static void main(String[] args)
+ {
+  System.out.println("  *PRINT HELLO 5 TIMES*");
+  int i=0;
+  do
+  {
+   System.out.println("Hello");
+   i++;
+  }while(i<5);
+ }
+}
